c0562e8b170c8505abfd38814ef9cf0f34aa97d0
curio13 <manishpndy527@gmail.com>
2024-02-27 17:51:31 +0530

    this is the last commit

--- mern-github-app-master/backend/server.js
+++ mern-github-app-master/backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import passport from "passport";
 import session from "express-session";
 import path from "path";
@@ -19,10 +20,13 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
-
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
+// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
